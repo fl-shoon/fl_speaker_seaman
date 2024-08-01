a9dbbc4dac5ac291b7a99f126272d8e7b1dcfdc9
Shun <shoon@framelunch.jp>
2024-08-01 23:02:19 +0900

    code modification

--- display/show.py
+++ display/show.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from transmission.serialModule import SerialModule
-from audio.playAudio import AudioPlayerModule
+from audio.audioPlayer import AudioPlayerModule
 from etc.define import *
 
 from PIL import Image
