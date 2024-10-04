f2383341d876f48b3aeacd903f99241cc708e6a5
Shun <shoon@framelunch.jp>
2024-10-04 21:12:05 +0900

    update

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ class VoiceAssistant:
                 break
 
             self.display.start_listening_display(SatoruHappy)
-            audio_data = self.interactive_recorder.record_question(silence_duration=2.5, max_duration=30, audio_player=self.audioPlayer)
+            audio_data = self.interactive_recorder.record_question(silence_duration=1.5, max_duration=30, audio_player=self.audioPlayer)
 
             if not audio_data:
                 silence_count += 1
